1a731e01b61e390908d597a4ea6fefc06fab306f
Andrew-dos-Anjos <andrewdosanjos2@gmail.com>
2025-12-14 22:45:41 -0300

    Venv e Executaveis
    Ambiente .venv + executaveis em python

---
+++ 19-Executavel_com_pyinstaller/A77.py
@@ -0,0 +1,14 @@
+import os
+
+os.system('msg * "Olá, mundo!"') # Windows
+os.system('notify-send "Olá, mundo!" "Esta é uma mensagem de teste"') # Linux
+
+# source .venv/bin/activate  # Linux (cd pasta_exata)
+# pip install pyinstaller
+# pyinstaller --onefile arquivo.py
+# Dist
+
+# --icon=icon.ico
+# https://icon-icons.com/search?q=python
+
+# --noconsole
---
+++ 30-Venv_Ambiente_Virtual/30-Venv_Ambiente_Virtual/A116.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# 1. Listar packages: pip freeze > requirements.txt
+# 2. Venv (Virtual environment): python -m venv .venv
+# 3. Ler requisitos: pip install -r requirements.txt
