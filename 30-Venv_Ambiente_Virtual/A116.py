ecb1fa0bf1d8d19ddaca7f1132d6350c212c0033
Andrew-dos-Anjos <andrewdosanjos2@gmail.com>
2026-03-11 15:23:27 -0300

    Update A116.py
    Solução de um problema com a .venv no windows

--- 30-Venv_Ambiente_Virtual/A116.py
+++ 30-Venv_Ambiente_Virtual/A116.py
@@ -3,3 +3,7 @@ import pandas as pd
 # 1. Listar packages: pip freeze > requirements.txt
 # 2. Venv (Virtual environment): python -m venv .venv
 # 3. Ler requisitos: pip install -r requirements.txt
+
+# Caso a maquina não esteja permitindo selecionar a .venv no windows:
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+# & .\.venv\Scripts\Activate.ps1
